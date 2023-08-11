25fef51484034020c2b018b58e237e2b79e40b17
Lucas Alber <lucasd.alber@gmail.com>
2023-08-11 13:38:33 +0200

    Light Cache: Test level interpolation

--- src/quake/light_cache.glsl
+++ src/quake/light_cache.glsl
@@ -8,9 +8,18 @@
 #define LIGHT_CACHE_MIN_ALPHA .01
 
 
-uint lc_level_for_pos(const vec3 pos) {
+uint lc_level_for_pos(const vec3 pos, inout uint rng_state) {
     const float target_grid_width = clamp(2 * LIGHT_CACHE_TAN_ALPHA_HALF * distance(pos, params.cam_x.xyz), LIGHT_CACHE_MIN_GRID_WIDTH, LIGHT_CACHE_MAX_GRID_WIDTH);
-    return uint(LIGHT_CACHE_LEVELS * pow((target_grid_width - LIGHT_CACHE_MIN_GRID_WIDTH) / (LIGHT_CACHE_MAX_GRID_WIDTH - LIGHT_CACHE_MIN_GRID_WIDTH), 1 / 9.));
+    const float level = LIGHT_CACHE_LEVELS * pow((target_grid_width - LIGHT_CACHE_MIN_GRID_WIDTH) / (LIGHT_CACHE_MAX_GRID_WIDTH - LIGHT_CACHE_MIN_GRID_WIDTH), 1 / 9.);
+    return uint(round(level));
+
+    // const float frac = fract(level);
+    // if (frac < .4 || frac > .6) {
+    //     return uint(round(level));
+    // }
+
+    // const uint lower = uint(level);
+    // return XorShift32(rng_state) < frac ? lower + 1 : lower;
 }
 
 ivec3 lc_grid_idx_for_level_closest(const uint level, const vec3 pos, inout uint rng_state) {
@@ -20,7 +29,6 @@ ivec3 lc_grid_idx_for_level_closest(const uint level, const vec3 pos, inout uint
 
 ivec3 lc_grid_idx_for_level_interpolate(const uint level, const vec3 pos, inout uint rng_state) {
     const float grid_width = pow(level / LIGHT_CACHE_LEVELS, 9.) * (LIGHT_CACHE_MAX_GRID_WIDTH - LIGHT_CACHE_MIN_GRID_WIDTH) + LIGHT_CACHE_MIN_GRID_WIDTH;
-    //return grid_idx_closest(pos, grid_width);
     return grid_idx_interpolate(pos, grid_width, XorShift32(rng_state));
 }
 
@@ -40,13 +48,8 @@ vec4 light_cache_get_level(const uint level, const vec3 pos, const vec3 normal, 
 }
 
 vec4 light_cache_get(const vec3 pos, const vec3 normal, inout uint rng_state) {
-    // const uint l = 5;
-    // const ivec3 grid_idx = grid_idx_for_level(l, pos, normal, rng_state);
-    // uint buf_idx = hash_grid_normal_level(grid_idx, normal, l, LIGHT_CACHE_BUFFER_SIZE);
-    // return vec4(XorShift32(buf_idx), XorShift32(buf_idx), XorShift32(buf_idx), XorShift32(buf_idx));
-
     {
-        const uint level = lc_level_for_pos(pos);
+        const uint level = lc_level_for_pos(pos, rng_state);
         return light_cache_get_level(level, pos, normal, rng_state);
     }
 
@@ -54,7 +57,7 @@ vec4 light_cache_get(const vec3 pos, const vec3 normal, inout uint rng_state) {
 }
 
 void light_cache_update(const vec3 pos, const vec3 normal, const vec3 irr, inout uint rng_state) {
-    const uint level = lc_level_for_pos(pos);
+    const uint level = lc_level_for_pos(pos, rng_state);
     const ivec3 grid_idx = lc_grid_idx_for_level_closest(level, pos, rng_state);
     const uint buf_idx = hash_grid_normal_level(grid_idx, normal, level, LIGHT_CACHE_BUFFER_SIZE);
     
