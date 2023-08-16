1befb6d26980d9043f5fc04aef66cae0df90e031
Lucas Alber <lucasd.alber@gmail.com>
2023-08-16 10:05:45 +0200

    Remove dead code

--- src/quake/mc.glsl
+++ src/quake/mc.glsl
@@ -50,26 +50,6 @@ vec4 mc_state_get_vmf(const MCState mc_state, const vec3 pos) {
     return vec4(mc_state_dir(mc_state, pos), (3.0 * r - r * r * r) / (1.0 - r * r));
 }
 
-// MCState mc_state_load(const vec3 pos, const vec3 normal, inout uint rng_state) {
-//     const ivec3 grid_idx = grid_idx_interpolate(pos, MC_GRID_WIDTH, XorShift32(rng_state));
-//     const uint buf_idx = hash_grid_normal(grid_idx, normal, MC_BUFFER_SIZE);
-//     const uint state_idx = uint(round(XorShift32(rng_state) * (STATES_PER_CELL - 1)));
-//     const MCState state = mc_states[buf_idx].states[uint(round(XorShift32(rng_state) * (STATES_PER_CELL - 1)))];
-//     if (grid_idx == state.grid_idx) {
-//         return state;
-//     } else {
-//         // const ivec3 grid_idx = grid_idx_interpolate(pos, MC_GRID_WIDTH, XorShift32(rng_state));
-//         // const uint buf_idx = hash_grid(grid_idx, MC_BUFFER_SIZE);
-//         // const uint state_idx = uint(round(XorShift32(rng_state) * (STATES_PER_CELL - 1)));
-//         // const MCState state = mc_states[buf_idx].states[uint(round(XorShift32(rng_state) * (STATES_PER_CELL - 1)))];
-//         // if (grid_idx == state.grid_idx) {
-//         //     return state;
-//         // } else {
-//             return mc_state_new();
-//         // }
-//     }
-// }
-
 // return true if a valid state was found
 bool mc_state_load_resample(out MCState mc_state, const vec3 pos, const vec3 normal, inout uint rng_state) {
     float score_sum = 0;
@@ -110,13 +90,6 @@ void mc_state_add_sample(inout MCState mc_state,
 }
 
 void mc_state_save(MCState mc_state, const vec3 pos, const vec3 normal, inout uint rng_state) {
-    // const ivec3 grid_idx = grid_idx_interpolate(pos, MC_GRID_WIDTH, XorShift32(rng_state));
-    // const uint buf_idx = hash_grid_normal(grid_idx, normal, MC_BUFFER_SIZE);
-    // const uint state_idx = uint(round(XorShift32(rng_state) * (STATES_PER_CELL - 1)));
-    // mc_state.grid_idx = grid_idx;
-    // //mc_state.normal = normal;
-    // mc_states[buf_idx].states[state_idx] = mc_state;
-    
     // update state that was used for sampling
     {
         const uint buf_idx = hash_grid_normal_level(mc_state.grid_idx, normal, mc_state.level, MC_BUFFER_SIZE);
@@ -136,38 +109,6 @@ void mc_state_save(MCState mc_state, const vec3 pos, const vec3 normal, inout ui
         mc_states[buf_idx].state = mc_state;
         mc_states[buf_idx].avg_frame = mix(mc_states[buf_idx].avg_frame, params.frame, .5);
     }
-
-    // for (int level = MC_MAX_LEVEL; level >= 0; level--) {
-    //     const ivec3 grid_idx = mc_grid_idx_for_level_interpolate(level, pos, normal, rng_state);
-    //     const uint buf_idx = hash_grid_normal_level(grid_idx, level, MC_BUFFER_SIZE);
-
-    //     const uint old = atomicExchange(mc_states[buf_idx].lock, params.frame);
-    //     if (old == params.frame)
-    //         // did not get lock
-    //         continue;
-
-    //     MCVertex vtx = mc_states[buf_idx];
-
-    //     if (old == 0) {
-    //         vtx.grid_idx = grid_idx;
-    //         vtx.level = level;
-    //         vtx.avg_frame = 0;
-    //     } else if (vtx.grid_idx == grid_idx && vtx.level == level) {
-            
-    //     } else if (XorShift32(rng_state) < level / vtx.level * .1) { // < level / vtx.level * .1
-    //         vtx.grid_idx = grid_idx;
-    //         vtx.level = level;
-    //         vtx.avg_frame = 0;
-    //     } else {
-    //         continue;
-    //     }
-
-    //     vtx.state = mc_state;
-    //     vtx.avg_frame = mix(vtx.avg_frame, params.frame, .5);
-
-    //     mc_states[buf_idx] = vtx;
-    //     return;   
-    // }
 }
 
 bool mc_state_valid(const MCState mc_state) {
